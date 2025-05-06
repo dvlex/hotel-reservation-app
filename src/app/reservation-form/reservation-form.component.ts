d252d5a6cde2ae4a9529e7bca9a5060e9fffd966
Alex <alex@lexdrel.com>
2025-05-06 20:04:14 +0000

    service and localstorage implemented

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
   selector: 'app-reservation-form',
@@ -6,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder,  private reservationService: ReservationService) {
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      this.reservationService.addReservation(this.reservationForm.value);
+    } else {
+      alert('Please fill in all required fields.');
+    }
+  }
 
 }
